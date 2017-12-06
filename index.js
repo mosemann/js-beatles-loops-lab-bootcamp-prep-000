2025a6ddc560a3791cddc1fcd2552fb1eab593cd
Joel Mosemann <mosemann@hotmail.com>
2017-12-06 23:03:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 function theBeatlesPlay(musicians, instruments) {
   var array = []
   for (let i = 0; i < musicians.length; i++) {
+    arra.push()
   if (i === 1) {a.push("I am 1 strange loop.")}
   else {a.push("I am ${i} strange loops.")}
 }return a
