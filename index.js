edf2a3dc1ffcba25b89607af0c6431a06c83978f
Joel Mosemann <mosemann@hotmail.com>
2017-12-07 00:02:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function johnLennonFacts(facts) {
   let countdown = facts.length
   while (countdown > 0) {
     facts[countdown] = facts[countdown] + "!!!"
-    countdown = countdown - 1
+    countdown--
   }
     return facts
 }
