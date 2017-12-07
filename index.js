05160ac61735a01c412e7f5d8bbdff78aeb32d35
Joel Mosemann <mosemann@hotmail.com>
2017-12-07 00:00:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ function theBeatlesPlay(musicians, instruments) {
 function johnLennonFacts(facts) {
   let countdown = facts.length
   while (countdown > 0) {
-    facts[countdown] = facts[countdown] + "!!!"
-    countdown = countdown - 1
+    facts[countdown] = facts[countdown] + "!!!";
+    countdown = countdown - 1;
   }
     return facts
 }
