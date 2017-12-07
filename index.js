bcebc4018fb20d59623761aec9f24d992b3a0e87
Joel Mosemann <mosemann@hotmail.com>
2017-12-07 00:24:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,5 +15,10 @@ function johnLennonFacts(facts) {
 }
 function iLoveTheBeatles(n) {
   var array =[]
-  
+  do {array.push("I love the Beatles!")
+    n++
+  }
+  while (n < 15)
+  return array
+
 }
